930ecc7779b3b6d591670b49f737ac27c1a2cf5c
Jakub Pietrus <pietrusjakub@gmail.com>
2022-08-09 00:09:46 +0200

    Add bottom homepage bar

---
+++ src/components/App.js
@@ -0,0 +1,102 @@
+import Header from "./ui/Header";
+import React from "react";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./ui/Theme";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import InputAdornment from "@mui/material/InputAdornment";
+import SearchIcon from "@mui/icons-material/Search";
+import { Icon, Typography } from "@mui/material";
+import GroupsIcon from "@mui/icons-material/Groups";
+import SportsKabaddiIcon from "@mui/icons-material/SportsKabaddi";
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Header />
+      <Grid
+        container
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        spacing={4}
+      >
+        <Grid item>
+          <Box
+            sx={{
+              width: 700,
+              maxWidth: "100%",
+            }}
+          >
+            <TextField
+              id="fullWidth"
+              fullWidth
+              placeholder="Find your new match"
+              variant="standard"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <SearchIcon />
+                  </InputAdornment>
+                ),
+              }}
+            />
+          </Box>
+        </Grid>
+        <Grid item>Mapbox</Grid>
+        <Grid item xs="12" sx={{ width: "100%" }}>
+          <Grid
+            container
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+            sx={{
+              height: 150,
+              backgroundColor: "primary.light",
+            }}
+          >
+            <Grid
+              item
+              sx={{
+                mx: 5,
+                display: "flex",
+                alignItems: "center",
+                flexWrap: "wrap",
+              }}
+            >
+              <SearchIcon sx={{ fontSize: "60px", mr: 2 }} />
+              <Typography variant="h2">FIND COURTS</Typography>
+            </Grid>
+            <Grid
+              item
+              sx={{
+                mx: 5,
+                display: "flex",
+                alignItems: "center",
+                flexWrap: "wrap",
+              }}
+            >
+              <GroupsIcon sx={{ fontSize: "60px", mr: 2 }} />
+              <Typography variant="h2">FIND TEEMATES</Typography>
+            </Grid>
+            <Grid
+              item
+              sx={{
+                mx: 5,
+                display: "flex",
+                alignItems: "center",
+                flexWrap: "wrap",
+              }}
+            >
+              <SportsKabaddiIcon sx={{ fontSize: "60px", mr: 2 }} />
+              <Typography variant="h2">PLAY GAME</Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
+  );
+}
+
+export default App;
