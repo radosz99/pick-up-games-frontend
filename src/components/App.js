6a88abcf3c8489d12377571f04c07451551e4062
Jakub Pietrus <pietrusjakub@gmail.com>
2022-08-09 14:48:28 +0200

    Add react-leaflet map

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import Header from "./ui/Header";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./ui/Theme";
 import Grid from "@mui/material/Grid";
@@ -7,11 +7,23 @@ import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import InputAdornment from "@mui/material/InputAdornment";
 import SearchIcon from "@mui/icons-material/Search";
-import { Icon, Typography } from "@mui/material";
+import { Typography } from "@mui/material";
 import GroupsIcon from "@mui/icons-material/Groups";
 import SportsKabaddiIcon from "@mui/icons-material/SportsKabaddi";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "../styles.css";
 
 function App() {
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      setLat(position.coords.latitude);
+      setLon(position.coords.longitude);
+    });
+  });
+
+  const [lat, setLat] = useState(null);
+  const [lon, setLon] = useState(null);
+
   return (
     <ThemeProvider theme={theme}>
       <Header />
@@ -44,15 +56,34 @@ function App() {
             />
           </Box>
         </Grid>
-        <Grid item>Mapbox</Grid>
-        <Grid item xs="12" sx={{ width: "100%" }}>
+        <Grid
+          item
+          sx={{
+            width: "100vw",
+          }}
+        >
+          {lat !== null && (
+            <MapContainer center={[lat, lon]} zoom={13} scrollWheelZoom={true}>
+              <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+              <Marker position={[lat, lon]}>
+                <Popup>
+                  A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+              </Marker>
+            </MapContainer>
+          )}
+        </Grid>
+        <Grid item sx={{ width: "100%" }}>
           <Grid
             container
             direction="row"
             justifyContent="center"
             alignItems="center"
             sx={{
-              height: 150,
+              height: 100,
               backgroundColor: "primary.light",
             }}
           >
@@ -66,7 +97,9 @@ function App() {
               }}
             >
               <SearchIcon sx={{ fontSize: "60px", mr: 2 }} />
-              <Typography variant="h2">FIND COURTS</Typography>
+              <Typography variant="h2" sx={{ color: "white" }}>
+                FIND COURTS
+              </Typography>
             </Grid>
             <Grid
               item
@@ -78,7 +111,9 @@ function App() {
               }}
             >
               <GroupsIcon sx={{ fontSize: "60px", mr: 2 }} />
-              <Typography variant="h2">FIND TEEMATES</Typography>
+              <Typography variant="h2" sx={{ color: "white" }}>
+                FIND TEAMMATES
+              </Typography>
             </Grid>
             <Grid
               item
@@ -90,7 +125,9 @@ function App() {
               }}
             >
               <SportsKabaddiIcon sx={{ fontSize: "60px", mr: 2 }} />
-              <Typography variant="h2">PLAY GAME</Typography>
+              <Typography variant="h2" sx={{ color: "white" }}>
+                PLAY GAME
+              </Typography>
             </Grid>
           </Grid>
         </Grid>
