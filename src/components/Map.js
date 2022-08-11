479592cbd01c0f934ec085b368db8dad9316411e
Jakub Pietrus <pietrusjakub@gmail.com>
2022-08-11 15:19:42 +0200

    Add button for getting location

--- src/components/Map.js
+++ src/components/Map.js
@@ -9,6 +9,7 @@ import React, { useEffect, useState } from "react";
 import SearchField from "./SearchField";
 import "leaflet/dist/leaflet.css";
 import Leaflet from "leaflet";
+import { Button } from "@mui/material";
 
 const AddMarker = () => {
   const [position, setPosition] = useState(null);
@@ -49,18 +50,53 @@ export default function Map() {
     });
   }, []);
 
+  function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(savePosition, showError);
+    } else {
+      console.log("Geolocation is not supported by this browser.");
+    }
+  }
+
+  function savePosition(position) {
+    setLat(position.coords.latitude);
+    setLon(position.coords.longitude);
+  }
+
+  function showError(error) {
+    switch (error.code) {
+      case error.PERMISSION_DENIED:
+        x.innerHTML = "User denied the request for Geolocation.";
+        break;
+      case error.POSITION_UNAVAILABLE:
+        x.innerHTML = "Location information is unavailable.";
+        break;
+      case error.TIMEOUT:
+        x.innerHTML = "The request to get user location timed out.";
+        break;
+      case error.UNKNOWN_ERROR:
+        x.innerHTML = "An unknown error occurred.";
+        break;
+    }
+  }
+
   return (
     <>
-      <MapContainer center={[12, 45]} zoom={13} scrollWheelZoom={true}>
-        <TileLayer url="https://tile.thunderforest.com/atlas/{z}/{x}/{y}.png?apikey=4a73bc6859bf49d089f11fef85911536" />
-        <SearchField />
-        {addCourtFlag && <AddMarker />}
-        <Marker position={[12, 45]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      </MapContainer>
+      <div>
+        <Button onClick={getLocation}>Get location</Button>
+      </div>
+      {lat && (
+        <MapContainer center={[lat, lon]} zoom={13} scrollWheelZoom={true}>
+          <TileLayer url="https://tile.thunderforest.com/atlas/{z}/{x}/{y}.png?apikey=4a73bc6859bf49d089f11fef85911536" />
+          <SearchField />
+          {addCourtFlag && <AddMarker />}
+          <Marker position={[lat, lon]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+      )}
     </>
   );
 }
