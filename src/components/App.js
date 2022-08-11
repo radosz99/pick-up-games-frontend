facb0e74301731f59bc71de696778543e8bd36cd
Jakub Pietrus <pietrusjakub@gmail.com>
2022-08-11 16:58:53 +0200

    Set default location to Wrocław. Add filters box. Add add court btn.

--- src/components/App.js
+++ src/components/App.js
@@ -7,16 +7,47 @@ import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import InputAdornment from "@mui/material/InputAdornment";
 import SearchIcon from "@mui/icons-material/Search";
-import { Typography } from "@mui/material";
+import { Typography, Modal } from "@mui/material";
 import GroupsIcon from "@mui/icons-material/Groups";
 import SportsKabaddiIcon from "@mui/icons-material/SportsKabaddi";
 import "../styles.css";
 import Map from "./Map";
 
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
 function App() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
     <ThemeProvider theme={theme}>
       <Header />
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
       <Grid
         container
         direction="column"
@@ -30,7 +61,7 @@ function App() {
             width: "100vw",
           }}
         >
-          <Map />
+          <Map handleOpenAddCourtModal={handleOpen} />
         </Grid>
         <Grid item sx={{ width: "100%" }}>
           <Grid
--- src/components/Map.js
+++ src/components/Map.js
@@ -4,88 +4,108 @@ import {
   Marker,
   Popup,
   useMapEvents,
+  ZoomControl,
 } from "react-leaflet";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import SearchField from "./SearchField";
 import { Button } from "@mui/material";
-import { VerticalAlignBottom } from "@mui/icons-material";
+import Control from "react-leaflet-custom-control";
+import Box from "@mui/material/Box";
+import Checkbox from "@mui/material/Checkbox";
+import FormControlLabel from "@mui/material/FormControlLabel";
 
-const AddMarker = () => {
-  const [position, setPosition] = useState(null);
-
-  useMapEvents({
-    click: (e) => {
-      setPosition(e.latlng); // 👈 add marker
-
-      /* CODE TO ADD NEW PLACE TO STORE (check the source code) */
-    },
-  });
-
-  return position === null ? null : <Marker position={position}></Marker>;
-};
-
-export default function Map() {
-  const [lat, setLat] = useState(null);
-  const [lon, setLon] = useState(null);
+export default function Map({ handleOpenAddCourtModal }) {
+  // const [lat, setLat] = useState(null);
+  // const [lon, setLon] = useState(null);
+  const [markers, setMarkers] = useState([[0, 0]]);
   const [addCourtFlag, setAddCourtFlag] = useState(false);
 
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(function (position) {
-      setLat(position.coords.latitude);
-      setLon(position.coords.longitude);
-      console.log(position.coords.latitude);
-      console.log(position.coords.longitude);
-      setAddCourtFlag(false); //TODO : REMOVE
+  const AddMarker = () => {
+    const [position, setPosition] = useState(null);
+
+    useMapEvents({
+      click: (e) => {
+        setPosition(e.latlng); // 👈 add marker
+        /* CODE TO ADD NEW PLACE TO STORE (check the source code) */
+        setMarkers([...markers, [e.latlng.lat, e.latlng.lng]]);
+        setAddCourtFlag(false);
+        handleOpenAddCourtModal();
+      },
     });
-  }, []);
 
-  function getLocation() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(savePosition, showError);
-    } else {
-      console.log("Geolocation is not supported by this browser.");
-    }
-  }
-
-  function savePosition(position) {
-    setLat(position.coords.latitude);
-    setLon(position.coords.longitude);
-  }
-
-  function showError(error) {
-    switch (error.code) {
-      case error.PERMISSION_DENIED:
-        console.log("User denied the request for Geolocation.");
-        break;
-      case error.POSITION_UNAVAILABLE:
-        console.log("Location information is unavailable.");
-        break;
-      case error.TIMEOUT:
-        console.log("The request to get user location timed out.");
-        break;
-      case error.UNKNOWN_ERROR:
-        console.log("An unknown error occurred.");
-        break;
-    }
-  }
+    return position === null ? null : <Marker position={position}></Marker>;
+  };
 
   return (
-    <>
-      <div>
-        <Button onClick={getLocation}>Get location</Button>
-      </div>
-      {lat && (
-        <MapContainer center={[lat, lon]} zoom={13} scrollWheelZoom={true}>
-          <TileLayer url="https://tile.thunderforest.com/atlas/{z}/{x}/{y}.png?apikey=4a73bc6859bf49d089f11fef85911536" />
-          <SearchField />
-          {addCourtFlag && <AddMarker />}
-          <Marker position={[lat, lon]}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-          </Marker>
-        </MapContainer>
-      )}
-    </>
+    <MapContainer
+      center={[51.109175, 17.032684]}
+      zoom={13}
+      zoomControl={false}
+      scrollWheelZoom={true}
+    >
+      <TileLayer url="https://tile.thunderforest.com/atlas/{z}/{x}/{y}.png?apikey=4a73bc6859bf49d089f11fef85911536" />
+      <ZoomControl position={"bottomleft"} />
+      <SearchField />
+      <Control prepend position="topleft">
+        <Button
+          variant="contained"
+          size="large"
+          color="primary"
+          onClick={(e) => {
+            e.stopPropagation();
+            setAddCourtFlag(!addCourtFlag);
+          }}
+          sx={{ mt: 5, ml: 5 }}
+        >
+          Add court
+        </Button>
+        <Box
+          sx={{
+            backgroundColor: "#E3F2FD",
+            p: 2,
+            mt: 40,
+            border: 15,
+            borderColor: "#E3F2FD",
+            borderRadius: 10,
+          }}
+        >
+          <Box>
+            <FormControlLabel
+              control={<Checkbox defaultChecked />}
+              label="Outdoor"
+            />
+            <br />
+            <FormControlLabel
+              control={<Checkbox defaultChecked />}
+              label="Indoor"
+            />
+          </Box>
+          <Box sx={{ mt: 5 }}>
+            <FormControlLabel
+              control={<Checkbox defaultChecked />}
+              label="With players today"
+            />
+            <br />
+            <FormControlLabel
+              control={<Checkbox defaultChecked />}
+              label="With photos"
+            />
+            <br />
+            <FormControlLabel
+              control={<Checkbox defaultChecked />}
+              label="Highly rated"
+            />
+          </Box>
+        </Box>
+      </Control>
+      {addCourtFlag && <AddMarker />}
+      {markers.map((position, idx) => (
+        <Marker key={`marker-${idx}`} position={position}>
+          <Popup>
+            <span>This will be court description.</span>
+          </Popup>
+        </Marker>
+      ))}
+    </MapContainer>
   );
 }
