a62426b18853ab2fefda1c9314f721a614fb8856
Jakub Pietrus <pietrusjakub@gmail.com>
2022-08-13 15:02:12 +0200

    Add icons bar in court details modal

--- src/components/CourtDetailsModal.js
+++ src/components/CourtDetailsModal.js
@@ -8,7 +8,7 @@ import Rating from "@mui/material/Rating";
 import NavigationIcon from "@mui/icons-material/Navigation";
 import PeopleAltIcon from "@mui/icons-material/PeopleAlt";
 import Carousel from "react-material-ui-carousel";
-import NavigateNextIcon from "@mui/icons-material/NavigateNext";
+import { Icon } from "@iconify/react";
 
 const style = {
   position: "absolute",
@@ -34,7 +34,7 @@ var items = [
 ];
 
 function Item(props) {
-  return <img height={300} alt="court" src={props.item.imgUrl} />;
+  return <img height={200} alt="court" src={props.item.imgUrl} />;
 }
 
 function CourtDetailsModal() {
@@ -122,21 +122,85 @@ function CourtDetailsModal() {
         <Grid
           container
           direction="row"
-          justifyContent="space-between"
+          justifyContent="center"
           alignItems="center"
           sx={{ mt: 4 }}
         >
           <Grid
             item
+            xs={3}
             sx={{
               display: "flex",
               alignItems: "center",
             }}
           >
-            <Rating name="read-only" value={4} readOnly />
-            <Typography component="legend">2 ratings</Typography>
+            <Icon icon="icon-park:court" rotate="90deg" height={60} />
+            <Grid
+              container
+              direction="column"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <Grid item>
+                <Typography>Number of courts</Typography>
+              </Grid>
+              <Grid item>
+                <Typography>1</Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid
+            item
+            xs={3}
+            sx={{
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <Icon icon="mdi:basketball-hoop-outline" height={60} />
+            <Grid
+              container
+              direction="column"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <Grid item>
+                <Typography>Number of hoops</Typography>
+              </Grid>
+              <Grid item>
+                <Typography>1</Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid
+            item
+            xs={3}
+            sx={{
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <Icon icon="material-symbols:table-lamp" height={60} />
+            <Grid
+              container
+              direction="column"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <Grid item>
+                <Typography>Lightning</Typography>
+              </Grid>
+              <Grid item>
+                <Typography>n/a</Typography>
+              </Grid>
+            </Grid>
           </Grid>
         </Grid>
+        <Box sx={{ mt: 5, textAlign: "center" }}>
+          <Typography sx={{ color: "blue" }}>
+            TODO: TIMELINE SLIDER COMPONENT
+          </Typography>
+        </Box>
       </Box>
     </Modal>
   );
